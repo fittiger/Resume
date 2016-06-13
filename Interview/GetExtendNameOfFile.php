c82238666734ec6a67eb535bd35232b1548a848b
fittiger <snakesoft@126.com>
2016-06-13 19:57:45 +0800

    匹配文件拓展名

---
+++ Interview/GetExtendNameOfFile.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: hasee
+ * Date: 2016/6/13
+ * Time: 19:49
+ */
+
+function getExt($url){
+    $arr=parse_url($url);
+    $file=basename($arr['path']);
+    $ext=explode('.',$file);
+    return $ext[1];
+}
+
+echo getExt('www.sohu.com/index.php');
